5e4f9a75a05b5889a127d99dd650ffabf73c0c65
momchiloiliev <momchilo.iliev@students.finki.ukim.mk>
2023-04-24 03:06:48 +0200

    Pagination

--- web/src/main/java/mk/ukim/finki/emt/eshop/web/rest/DiscountRestController.java
+++ web/src/main/java/mk/ukim/finki/emt/eshop/web/rest/DiscountRestController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/api/discounts")
 public class DiscountRestController {//crud funkcionalnost->create,read,update,delete
 
